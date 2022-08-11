db79daff2a67b597a0a95e827e7ef93cbeca4666
norio6199 <norio6199@gmail.com>
2022-08-11 22:47:11 +0900

    setting emotion

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,9 +1,14 @@
+import { css } from "@emotion/react";
 import React from "react";
 
 export const Home: React.FC = () => {
   return (
-    <section>
+    <section css={style}>
       <h1>Home</h1>
     </section>
   );
 };
+
+const style = css({
+  color: "green",
+});
